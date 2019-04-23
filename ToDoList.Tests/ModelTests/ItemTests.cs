eada7a1264652963912b3f9b8e6b074bc03e263a
Maryana Antonyuk and Sarah Angel <sarah@>
2019-04-23 16:21:36 -0700

    add methods,static,instances

---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,64 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+using System.Collections.Generic;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ItemTest :IDisposable
+  {
+    public void Dispose()
+{
+  Item.ClearAll();
+}
+  [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+    [TestMethod]
+    public void GetDescription_ReturnDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      string result = newItem.GetDescription();
+
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      string updatedDesription = "Do the dishes";
+      newItem.SetDescription(updatedDesription);
+      string result = newItem.GetDescription();
+
+      Assert.AreEqual(updatedDesription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnEmptyList_ItemList()
+    {
+      List<Item> newList = new List<Item> { };
+      List<Item> result = Item.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2};
+      List<Item> result = Item.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+  }
+}
